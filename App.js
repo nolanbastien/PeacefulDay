95273de44dc7bc4a9fdccc043a5c68c2000f8a7c
Nolan Bastien <n_astien@live.concordia.ca>
2022-02-12 11:45:29 -0500

    Add NativeBase Dependencies

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, Box, Button } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Hello World</Text>
+      </Box>
+      <Box alignItems="center">
+        <Button onPress={() => console.log("hello world")}>Click Me</Button>
+      </Box>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });
